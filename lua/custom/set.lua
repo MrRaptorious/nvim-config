d7a2c60986714ab45fa4415c3d85fa600add4674
Lorenz Braun <lorenz.schmitten@gmail.com>
2023-09-30 02:39:52 +0200

    use kickstart

--- lua/custom/set.lua
+++ lua/custom/set.lua
@@ -1,12 +1,10 @@
 vim.opt.guicursor = ""
 
-vim.opt.nu = true
-vim.opt.relativenumber = true
-
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
+vim.opt.smartindent = true
 
 vim.opt.smartindent = true
 
@@ -29,4 +27,3 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
-
